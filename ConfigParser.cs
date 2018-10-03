0770411ca74888f95cffad2cdcf727a311d66759
demderk <damdark107@gmail.com>
2018-10-03 18:58:18 +0700

    Release 1.0

---
+++ ConfigParser.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO.Ports;
+using System.Xml;
+
+namespace DirectTerminal
+{
+    public enum LineEnding { Nope, NL, CR };
+
+    public struct PortInfo
+    {
+        public SerialPort sPort;
+        public LineEnding lEnd;
+    }
+
+    class ConfigParser
+    {
+
+        private string COMPortName { get; set; } = null;
+
+        private int PortBaudRate { get; set; } = -1;
+
+        private Parity PortParity { get; set; } = Parity.None;
+
+        private int PortDataBits { get; set; } = 8;
+
+        private StopBits PortStopBits { get; set; } = StopBits.One;
+
+        private LineEnding PortLineEnding { get; set; } = LineEnding.NL;
+
+        /// <summary>
+        /// Read xml file.
+        /// </summary>
+        /// <param name="file">XmlDocument class with loaded file.</param>
+        private void ReadXML(XmlDocument file)
+        {
+            //string errObject = "";
+            XmlElement config = file.DocumentElement;
+            foreach (XmlNode node in config.ChildNodes)
+            {
+                switch (node.Name)
+                {
+                    case "COMPort":
+                        //errObject = "COMPort";
+                        if (node.Attributes["question"] != null && node.Attributes["question"].Value == "true")
+                        {
+                            Console.WriteLine("Config manager: Type COM port name");
+                            COMPortName = Console.ReadLine();
+                        }
+                        else
+                        {
+                            COMPortName = node.InnerText;
+                        }
+                        break;
+                    case "BaudRate":
+                        //errObject = "BaudRate";
+                        PortBaudRate = Convert.ToInt32(node.InnerText);
+                        break;
+                    case "Parity":
+                        //errObject = "Parity";
+                        PortParity = (Parity)Enum.Parse(typeof(Parity), node.InnerText);
+                        break;
+                    case "DataBits":
+                        //errObject = "DataBits";
+                        PortDataBits = Convert.ToInt32(node.InnerText);
+                        break;
+                    case "StopBits":
+                        //errObject = "StopBits";
+                        PortStopBits = (StopBits)Enum.Parse(typeof(StopBits), node.InnerText);
+                        break;
+                    case "LineEnding":
+                        //errObject = "LineEnding";
+                        PortLineEnding = (LineEnding)Enum.Parse(typeof(LineEnding), node.InnerText);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// User presets setting.
+        /// </summary>
+        /// <param name="path">Path of xml file.</param>
+        /// <returns></returns>
+        public bool UserPresetsEnabled(string path)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(path);
+            XmlElement config = xmlDocument.DocumentElement;
+            foreach (XmlNode item in config)
+            {
+                if (item.Name == "DisableUserPreset")
+                {
+                    if (item.InnerText == "true")
+                    {
+                        return false;
+                    }
+                    else if (item.InnerText == "false")
+                    {
+                        return true;
+                    }
+                    throw new FormatException();
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// PortInfo class builder.
+        /// </summary>
+        /// <param name="file">Path of xml file</param>
+        /// <returns></returns>
+        public PortInfo BuildPort(XmlDocument file)
+        {
+            ReadXML(file);
+            return new PortInfo()
+            {
+                sPort = new SerialPort(COMPortName, PortBaudRate, PortParity, PortDataBits, PortStopBits),
+                lEnd = PortLineEnding
+            };
+        }
+    }
+}
---
+++ Logic.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectTerminal
+{
+    class Logic
+    {
+        private string portLineEndingString = "";
+
+        private LineEnding portLineEnding;
+
+        public LineEnding PortLineEnding
+        {
+            get
+            {
+                return portLineEnding;
+            }
+            set
+            {
+                portLineEnding = value;
+                switch (value)
+                {
+                    case LineEnding.Nope:
+                        portLineEndingString = "";
+                        break;
+                    case LineEnding.NL:
+                        portLineEndingString = "\n";
+                        break;
+                    case LineEnding.CR:
+                        portLineEndingString = "\r";
+                        break;
+                }
+            }
+        }
+
+        public bool IsCanceled { get; set; } = false;
+
+        public void DTermainalBody(SerialPort port)
+        // Main logic body
+        {
+
+            bool pause = false;
+
+            while (!IsCanceled)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo ski = Console.ReadKey(true);
+                    if (ski.Modifiers == ConsoleModifiers.Shift && ski.Key == ConsoleKey.P)
+                    {
+                        pause = !pause;
+                        // Turn on pause mode
+                        if (pause)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write("  PAUSED  ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            for (int i = 0; i < 10; i++)
+                            {
+                                Console.Write("\b");
+                                Console.Write(" ");
+                                Console.Write("\b");
+
+                            }
+                        }
+                    }
+                    if (!pause)
+                    {
+                        if (ski.Modifiers == ConsoleModifiers.Control && ski.Key == ConsoleKey.C)
+                        {
+                            Environment.Exit(0);
+                        }
+                        else if (ski.Modifiers == ConsoleModifiers.Shift && ski.Key == ConsoleKey.X)
+                        {
+                            string commandBuffer = "";
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Send> ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            commandBuffer = Console.ReadLine();
+                            for (int i = 0; i < commandBuffer.Length + 6; i++)
+                            {
+                                Console.Write("\b");
+                                Console.Write(" ");
+                                Console.Write("\b");
+                            }
+                            for (int i = 0; i < port.BytesToRead; i++)
+                            {
+                                Console.Write((char)port.ReadByte());
+                            }
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            port.Write($"{commandBuffer}{portLineEndingString}");
+                            Console.Write("SENDED: ");
+                            Console.WriteLine(commandBuffer);
+                            Console.ResetColor();
+                        }
+                        else if (ski.Modifiers == ConsoleModifiers.Shift && ski.Key == ConsoleKey.C)
+                        {
+                            Console.Clear();
+                            Draw drw = new Draw();
+                            drw.DrawHeader();
+                        }
+                    }
+                }
+                if (!pause && port.BytesToRead != 0)
+                {
+                    Console.Write((char)port.ReadByte());
+                }
+            }
+        }
+    }
+}
